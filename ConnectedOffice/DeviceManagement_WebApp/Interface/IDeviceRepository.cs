647d7054fb7c068b9022b542c7d96807af25b923
Manlx <74509303+Manlx@users.noreply.github.com>
2022-09-28 17:56:13 +0200

    Fixed not all data displaying

--- ConnectedOffice/DeviceManagement_WebApp/Interface/IDeviceRepository.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Interface/IDeviceRepository.cs
@@ -1,5 +1,7 @@
 ﻿using DeviceManagement_WebApp.Models;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DeviceManagement_WebApp.Interface
 {
@@ -7,5 +9,7 @@ namespace DeviceManagement_WebApp.Interface
     {
         //Looks if Entity of Type Device Exists with Guid of 
         bool DeviceExists(Guid id);
+        //Had to Create new GetAll method because base Get all method does not include correct data;
+        public Task<List<Device>> GetAllAll();
     }
 }
