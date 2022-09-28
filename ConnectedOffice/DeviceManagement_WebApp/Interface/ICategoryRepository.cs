ab74c10d878f5f7edb181caa372670f281501f57
Manlx <74509303+Manlx@users.noreply.github.com>
2022-09-28 15:04:39 +0200

    Readding

---
+++ ConnectedOffice/DeviceManagement_WebApp/Interface/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using DeviceManagement_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DeviceManagement_WebApp.Interface
+{
+    public interface ICategoryRepository: IGenericRepository<Category>
+    {
+        //Add specialized Zone Methods
+        public bool CategoryExists(Guid id);
+
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/GenericRepository.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+using DeviceManagement_WebApp.Interface;
+using DeviceManagement_WebApp.Data;
+using System.Linq;
+using DeviceManagement_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ConnectedOfficeContext _context;
+
+        public GenericRepository(ConnectedOfficeContext context)
+        {
+            _context = context;
+        }
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public T GetById(Guid id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Update(T Entit)
+        {
+            _context.Update(Entit);
+        }
+        public Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().FirstOrDefaultAsync(expression);
+        }
+        public ConnectedOfficeContext GetContext()
+        {
+            return this._context;
+        }
+
+        public Task<List<T>> ToListAsync()
+        {
+            var Outs = _context.Set<T>().ToListAsync();
+            return Outs;
+        }
+        public async Task<T> FindAsync(Guid id)
+        {
+            var category =await _context.Set<T>().FindAsync(id);
+            return category;
+        }
+    }
+}
